9d4a69549645ba8a729e6d8686e021f2173cc8d0
JackWFinlay <Jack.Witbrock@gmail.com>
2014-12-16 17:19:43 +1300

    Update provisioning script with WinRM Rundeck plug-in.

--- provision.sh
+++ provision.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-sudo yum install -y java-1.6.0 
-sudo rpm -Uvh http://repo.rundeck.org/latest.rpm 
-sudo yum install -y rundeck 
+sudo yum install -y java-1.6.0
+sudo rpm -Uvh http://repo.rundeck.org/latest.rpm
+sudo yum install -y rundeck
+sudo yum install -y wget
+cd ../var/lib/rundeck/libext
+wget https://github.com/rundeck-plugins/rundeck-winrm-plugin/releases/download/v1.2/rundeck-winrm-plugin-1.2.jar
 sudo service rundeckd start
