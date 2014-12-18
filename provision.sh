5fad506e430e90d66b8e1e73261475847a04ddd1
Jack W Finlay <Jack.Witbrock@gmail.com>
2014-12-18 17:11:37 +1300

    fix download location error

--- provision.sh
+++ provision.sh
@@ -4,6 +4,6 @@ sudo yum install -y java-1.6.0
 sudo rpm -Uvh http://repo.rundeck.org/latest.rpm
 sudo yum install -y rundeck
 sudo yum install -y wget
-cd ../var/lib/rundeck/libext
+cd /var/lib/rundeck/libext
 wget https://github.com/rundeck-plugins/rundeck-winrm-plugin/releases/download/v1.2/rundeck-winrm-plugin-1.2.jar
 sudo service rundeckd start
